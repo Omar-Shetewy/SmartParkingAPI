9451958a4cfd09aacd8a2d147509c45a3274268f
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2025-03-18 23:54:19 +0200

    Add Global File

--- SmartParkingAPI/GlobalUsings.cs
+++ SmartParkingAPI/GlobalUsings.cs
@@ -10,3 +10,4 @@ global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using SmartParkingAPI.Data.DTO;
 global using SmartParkingAPI.Services;
+global using System.Reflection.Metadata;
