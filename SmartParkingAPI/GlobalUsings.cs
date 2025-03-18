c150cf65dea84ff2fb621104e66ee27f76b8ec0c
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2025-03-18 23:14:37 +0200

    Add Global File

---
+++ SmartParkingAPI/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using SmartParking.API.Data.Models;
+global using SmartParkingAPI.Data.Models;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.Data.SqlClient;
+global using Microsoft.Extensions.Configuration;
+global using SmartParkingAPI.Data;
+global using AutoMapper;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using SmartParkingAPI.Data.DTO;
+global using SmartParkingAPI.Services;
