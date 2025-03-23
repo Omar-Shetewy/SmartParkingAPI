8b3de7d5c87acdcbcc312d92c6d4979bd1612be2
Omar-Shetewy <omarshetewy10@gmail.com>
2025-03-24 00:54:27 +0200

    Nothing

---
+++ SmartParkingAPI/Migrations/20250323214314_UpdateReservationRecordsTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartParkingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReservationRecordsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GarageId",
+                table: "ReservationRecords",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationRecords_GarageId",
+                table: "ReservationRecords",
+                column: "GarageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReservationRecords_Garages_GarageId",
+                table: "ReservationRecords",
+                column: "GarageId",
+                principalTable: "Garages",
+                principalColumn: "GarageId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReservationRecords_Garages_GarageId",
+                table: "ReservationRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReservationRecords_GarageId",
+                table: "ReservationRecords");
+
+            migrationBuilder.DropColumn(
+                name: "GarageId",
+                table: "ReservationRecords");
+        }
+    }
+}
