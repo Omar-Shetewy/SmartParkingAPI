4029b6ba3863a1c7bf4f8fb49841ec79e69b1b99
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-03-24 23:34:18 +0200

    Add authentication

--- SmartParkingAPI/Program.cs
+++ SmartParkingAPI/Program.cs
@@ -1,3 +1,7 @@
+
+using SmartParking.API.Services.Implementation;
+using SmartParking.API.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,18 +10,36 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Authentication Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!)), // this ! means not null
+            ValidateIssuerSigningKey = true
+        };
+    });
+
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<ICarService, CarService>();
 builder.Services.AddTransient<IGarageService, GarageService>();
+builder.Services.AddScoped<IAuthService, AuthServicie>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
 var configuration = builder.Configuration;
 var connectionStrings = new Dictionary<string, string>
 {
-    { "MedoConnection", configuration.GetConnectionString("MedoConnection") },
-    { "RokaConnection", configuration.GetConnectionString("RokaConnection") },
-    { "AmorConnection", configuration.GetConnectionString("AmorConnection") }
+    //{ "MedoConnection", configuration.GetConnectionString("MedoConnection") },
+    { "RokaConnection", configuration.GetConnectionString("RokaConnection") }
+    //{ "AmorConnection", configuration.GetConnectionString("AmorConnection") }
 };
 
 string? activeConnection = connectionStrings.FirstOrDefault(c => IsDatabaseAvailable(c.Value)).Value;
@@ -38,12 +60,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
+    app.MapScalarApiReference();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
