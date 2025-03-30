19aa5209a8e5426dd2683e8d547a9d78e378574e
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-03-30 17:38:32 +0200

    update migrations

--- SmartParkingAPI/Migrations/20250330151828_AddingDefaultRoleValues.Designer.cs
+++ SmartParkingAPI/Migrations/20250330151828_AddingDefaultRoleValues.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartParkingAPI.Data;
 
@@ -11,9 +12,11 @@ using SmartParkingAPI.Data;
 namespace SmartParking.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250330151828_AddingDefaultRoleValues")]
+    partial class AddingDefaultRoleValues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
