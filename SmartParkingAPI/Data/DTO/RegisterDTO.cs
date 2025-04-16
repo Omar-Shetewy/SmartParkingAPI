8da3a394bf2f1601dd8472f30394930b4c55dcdf
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-04-16 12:01:35 +0200

    Deleting UserId properity

--- SmartParkingAPI/Data/DTO/RegisterDTO.cs
+++ SmartParkingAPI/Data/DTO/RegisterDTO.cs
@@ -6,7 +6,6 @@ namespace SmartParkingAPI.Data.DTO;
 
 public class RegisterDTO
 {
-    public int UserId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
