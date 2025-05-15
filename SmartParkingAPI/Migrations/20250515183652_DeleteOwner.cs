c1c148898f6f9a7de5f1951a01ca8fab556fd64d
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2025-05-15 21:38:55 +0300

    Delete Owner table

---
+++ SmartParkingAPI/Migrations/20250515183652_DeleteOwner.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartParking.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Garages_Owners_OwnerId",
+                table: "Garages");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Garages_OwnerId",
+                table: "Garages");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Garages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Garages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.OwnerId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Garages_OwnerId",
+                table: "Garages",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Garages_Owners_OwnerId",
+                table: "Garages",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "OwnerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
