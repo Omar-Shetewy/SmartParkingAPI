4d1aaa5b02be5d195fe623074aeff5739d87e051
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2025-05-17 19:11:40 +0300

    Refresh token reposatory

---
+++ SmartParkingAPI/Repositories/Implementation/RefreshTokenRepositories.cs
@@ -0,0 +1,31 @@
+﻿namespace SmartParking.API.Repositories.Implementation;
+
+public class RefreshTokenRepositories : IRefreshTokenRepositories
+{
+    private readonly ApplicationDbContext _context;
+
+    public RefreshTokenRepositories(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<RefreshToken?> GetByIdAsync(RefreshTokenDTO token)
+    {
+        return await _context.RefreshTokens.Include(t => t.User).ThenInclude(r => r.Role).FirstOrDefaultAsync(t => t.UserId == token.Id);
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(RefreshTokenDTO token)
+    {
+        return await _context.RefreshTokens.Include(t => t.User).ThenInclude(r => r.Role).FirstOrDefaultAsync(t => t.Token == token.Token);
+    }
+
+    public async Task AddAsync(RefreshToken token)
+    {
+        await _context.RefreshTokens.AddAsync(token);
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
