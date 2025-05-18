0871c19e52a187280967ba812af7aea6db010617
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2025-05-18 17:24:19 +0300

    Adding Automation

---
+++ SmartParkingAPI/Controllers/AutomationController.cs
@@ -0,0 +1,84 @@
+﻿namespace SmartParking.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AutomationController : ControllerBase
+{
+    private readonly IGarageService _garageService;
+    private readonly IMapper _mapper;
+    private readonly IConfiguration _config;
+    public AutomationController(IConfiguration config, IGarageService garageService, IMapper mapper)
+    {
+        _config = config;
+        _garageService = garageService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("Stream/urls")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetCameraUrls()
+    {
+        var serverIp = _config["MediaServerIp"];
+
+        var response = new
+        {
+            Camera1 = new
+            {
+                RTSP = $"rtsp://{serverIp}:8554/cam1",
+                HLS = $"http://{serverIp}:8888/cam1/index.m3u8"
+            },
+            Camera2 = new
+            {
+                RTSP = $"rtsp://{serverIp}:8554/cam2",
+                HLS = $"http://{serverIp}:8888/cam2/index.m3u8"
+            }
+        };
+
+        return Ok(new ApiResponse<object>(response, "Success", true));
+
+    }
+
+    [HttpPost]
+    [Route("AddEntryCar")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddEntryCarAsync([FromBody] EntryCarDTO entryCarDTO)
+    {
+
+        var isValidPlateNumber = await _garageService.isValidPlateNumber(entryCarDTO.PlateNumber);
+        //if (!isValidPlateNumber)
+        //    return BadRequest(new ApiResponse<object>(null, $"Invalid Plate Number {entryCarDTO.PlateNumber}", false));
+        if (entryCarDTO == null)
+            return BadRequest(new ApiResponse<object>(null, "Entry car data is required", false));
+        var isValidGarage = await _garageService.isValidGarage(entryCarDTO.GarageId);
+        if (!isValidGarage)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Garage ID {entryCarDTO.GarageId}", false));
+        var entryCar = _mapper.Map<EntryCar>(entryCarDTO);
+        var result = await _garageService.AddEntryCar(entryCar);
+        if (result == null)
+            return BadRequest(new ApiResponse<object>(null, "Failed to add entry car", false));
+
+        var entryCarDetails = _mapper.Map<EntryCarDetailsDTO>(result);
+
+        return Ok(new ApiResponse<EntryCarDetailsDTO>(entryCarDetails, "Success", true));
+
+    }
+
+    [HttpPut]
+    [Route("CarExit")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CarExit([FromBody] EntryCarDTO entryCarDTO)
+    {
+        if (entryCarDTO == null)
+            return BadRequest(new ApiResponse<object>(null, "Entry car data is required", false));
+        var isValidGarage = await _garageService.isValidGarage(entryCarDTO.GarageId);
+        if (!isValidGarage)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Garage ID {entryCarDTO.GarageId}", false));
+        var entryCar = _mapper.Map<EntryCar>(entryCarDTO);
+        var result = await _garageService.UpdateExitCar(entryCar.PlateNumber);
+        if (result == null)
+            return BadRequest(new ApiResponse<object>(null, "Failed to add entry car", false));
+        return Ok(new ApiResponse<EntryCar>(result, "Success", true));
+    }
+}
--- SmartParkingAPI/Controllers/CamerasController.cs
+++ SmartParkingAPI/Controllers/CamerasController.cs
@@ -1,152 +1,147 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SmartParking.API.Data.Models;
+﻿namespace SmartParking.API.Controllers;
 
-namespace SmartParking.API.Controllers
+[Route("api/Cameras")]
+[ApiController]
+public class CamerasController : ControllerBase
 {
-    [Route("api/Cameras")]
-    [ApiController]
-    public class CamerasController : ControllerBase
+    private readonly ICameraService _cameraService;
+    private readonly IGarageService _garageService;
+    private readonly IMapper _mapper;
+
+    public CamerasController(IMapper mapper, ICameraService cameraService, IGarageService garageService)
     {
-        private readonly ICameraService _cameraService;
-        private readonly IGarageService _garageService;
-        private readonly IMapper _mapper;
+        _mapper = mapper;
+        _cameraService = cameraService;
+        _garageService = garageService;
+    }
 
-        public CamerasController(IMapper mapper, ICameraService cameraService, IGarageService garageService)
-        {
-            _mapper = mapper;
-            _cameraService = cameraService;
-            _garageService = garageService;
-        }
+    [HttpGet]
+    [Route("GetAllCameras")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAllCarsAsync()
+    {
+        var cameras = await _cameraService.GetAll();
 
-        [HttpGet]
-        [Route("GetAllCameras")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> GetAllCarsAsync()
-        {
-            var cameras = await _cameraService.GetAll();
+        if (cameras.Count() == 0)
+            return NoContent();
 
-            if (cameras.Count() == 0)
-                return NoContent();
+        var data = _mapper.Map<List<CameraDetailsDTO>>(cameras);
 
-            var data = _mapper.Map<List<CameraDetailsDTO>>(cameras);
+        return Ok(new ApiResponse<List<CameraDetailsDTO>>(data, "", true));
+    }
 
-            return Ok(new ApiResponse<List<CameraDetailsDTO>>(data, "", true));
-        }
+    [HttpGet]
+    [Route("GetCameraByGarageId/{garageId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetCameraByGarageId(int garageId)
+    {
+        var isValidGarageId = await _garageService.isValidGarage(garageId);
 
-        [HttpGet]
-        [Route("GetCameraByGarageId/{garageId}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetCameraByGarageId(int garageId)
-        {
-            var isValidGarageId = await _garageService.isValidGarage(garageId);
+        if (!isValidGarageId || garageId < 1)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{garageId}", false));
 
-            if (!isValidGarageId || garageId < 1)
-                return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{garageId}", false));
+        var cameras = await _cameraService.GetByGarageId(garageId);
 
-            var cameras = await _cameraService.GetByGarageId(garageId);
+        if (cameras.Count() == 0)
+            return NoContent();
 
-            if (cameras.Count() == 0)
-                return NoContent();
+        var data = _mapper.Map<List<CameraDetailsDTO>>(cameras);
 
-            var data = _mapper.Map<List<CameraDetailsDTO>>(cameras);
+        return Ok(new ApiResponse<List<CameraDetailsDTO>>(data, "", true));
+    }
 
-            return Ok(new ApiResponse<List<CameraDetailsDTO>>(data, "", true));
-        }
+    [HttpGet]
+    [Route("GetGarageById/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetCameraById(int id)
+    {
+        if (id < 1)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Camera Id:{id}", false));
 
-        [HttpGet]
-        [Route("GetGarageById/{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetCameraById(int id)
-        {
-            if (id < 1)
-                return BadRequest(new ApiResponse<object>(null, $"Invalid Camera Id:{id}", false));
+        var camera = await _cameraService.GetBy(id);
 
-            var camera = await _cameraService.GetBy(id);
+        if (camera == null)
+            return NotFound(new ApiResponse<object>(null, $"Camera with id = {id} is not found", false));
 
-            if (camera == null)
-                return NotFound(new ApiResponse<object>(null, $"Camera with id = {id} is not found", false));
+        var data = _mapper.Map<CameraDetailsDTO>(camera);
 
-            var data = _mapper.Map<CameraDetailsDTO>(camera);
+        return Ok(new ApiResponse<CameraDetailsDTO>(data, "", true));
+    }
 
-            return Ok(new ApiResponse<CameraDetailsDTO>(data, "", true));
-        }
+    [HttpPost]
+    [Route("AddNewCamera")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddNewCarAsync([FromBody] CameraDTO dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ApiResponse<object>(ModelState, "", false));
 
-        [HttpPost]
-        [Route("AddNewCamera")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> AddNewCarAsync([FromBody] CameraDTO dto)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(new ApiResponse<object>(ModelState, "", false));
+        var isValidGarageId = await _garageService.isValidGarage(dto.GarageId);
 
-            var isValidGarageId = await _garageService.isValidGarage(dto.GarageId);
+        if (!isValidGarageId || dto.GarageId < 1)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{dto.GarageId}", false));
 
-            if (!isValidGarageId || dto.GarageId < 1)
-                return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{dto.GarageId}", false));
+        var camera = _mapper.Map<Camera>(dto);
+        await _cameraService.Add(camera);
 
-            var camera = _mapper.Map<Camera>(dto);
-            await _cameraService.Add(camera);
+        return Ok(new ApiResponse<Camera>(camera, "", true));
+    }
 
-            return Ok(new ApiResponse<Camera>(camera, "", true));
-        }
+    [HttpPut]
+    [Route("UpdateCamera/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateCarAsync(int id, [FromBody] CameraDTO dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ApiResponse<object>(ModelState, "", false));
 
-        [HttpPut]
-        [Route("UpdateCamera/{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> UpdateCarAsync(int id, [FromBody] CameraDTO dto)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(new ApiResponse<object>(ModelState, "", false));
+        var isValidGarageId = await _garageService.isValidGarage(dto.GarageId);
 
-            var isValidGarageId = await _garageService.isValidGarage(dto.GarageId);
+        if (!isValidGarageId || dto.GarageId < 1)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{dto.GarageId}", false));
 
-            if (!isValidGarageId || dto.GarageId < 1)
-                return BadRequest(new ApiResponse<object>(null, $"Invalid Garage Id:{dto.GarageId}", false));
+        var camera = await _cameraService.GetBy(id);
 
-            var camera = await _cameraService.GetBy(id);
+        if (camera == null)
+            return NotFound(new ApiResponse<object>(null, $"Camera with id {id} is not found!", false));
 
-            if (camera == null)
-                return NotFound(new ApiResponse<object>(null, $"Camera with id {id} is not found!", false));
+        camera.Name = dto.Name;
+        camera.GarageId = dto.GarageId;
 
-            camera.Name = dto.Name;
-            camera.GarageId = dto.GarageId;
-          
-            _cameraService.Update(camera);
+        _cameraService.Update(camera);
 
-            return Ok(new ApiResponse<Camera>(camera, "Updated Successfully", true));
+        return Ok(new ApiResponse<Camera>(camera, "Updated Successfully", true));
 
-        }
+    }
 
-        [HttpDelete]
-        [Route("DeleteCamera/{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteCameraAsync(int id)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(new ApiResponse<object>(ModelState, "", false));
+    [HttpDelete]
+    [Route("DeleteCamera/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteCameraAsync(int id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ApiResponse<object>(ModelState, "", false));
 
-            if (id < 1)
-                return BadRequest(new ApiResponse<object>(null, $"Invalid Camera Id:{id}", false));
+        if (id < 1)
+            return BadRequest(new ApiResponse<object>(null, $"Invalid Camera Id:{id}", false));
 
-            var camera = await _cameraService.GetBy(id);
+        var camera = await _cameraService.GetBy(id);
 
-            if (camera == null)
-                return NotFound(new ApiResponse<object>(null, $"Camera with id {id} is not found!", false));
+        if (camera == null)
+            return NotFound(new ApiResponse<object>(null, $"Camera with id {id} is not found!", false));
 
-            _cameraService.Delete(camera);
+        _cameraService.Delete(camera);
 
-            return Ok(new ApiResponse<Camera>(camera, "Deleted Successfully", true));
-        }
+        return Ok(new ApiResponse<Camera>(camera, "Deleted Successfully", true));
     }
 }
