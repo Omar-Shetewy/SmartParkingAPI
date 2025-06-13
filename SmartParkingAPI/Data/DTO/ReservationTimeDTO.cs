1561c99892e13b378d0ca74b149ab8f4a8f33b40
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-06-13 19:33:13 +0300

    Minor Updates

---
+++ SmartParkingAPI/Data/DTO/ReservationTimeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartParking.API.Data.DTO;
+
+public class ReservationTimeDTO
+{
+    public int ReservationId { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime EndDate { get; set; }
+}
