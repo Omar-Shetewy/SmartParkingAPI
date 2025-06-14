6290aef1986d225dd1bdc515b27413fe84c1e6e7
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-06-14 16:28:19 +0300

    Update Owners Table

--- SmartParkingAPI/Data/Models/Owner.cs
+++ SmartParkingAPI/Data/Models/Owner.cs
@@ -4,7 +4,7 @@ public class Owner
 {
     public int OwnerId { get; set; }
     public int UserId { get; set; }
-    //public User User { get; set; }
-    //public List<Garage> Garages { get; set; }
+    public User User { get; set; }
+    public List<Garage> Garages { get; set; }
 
 }
