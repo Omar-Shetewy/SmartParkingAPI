7ec0dd1b9abc507d7910809510a1e3e9a1e635a2
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2025-06-18 02:08:23 +0300

    Add OwnerId = null as a defalt value

--- SmartParkingAPI/Data/DTO/GarageDTO.cs
+++ SmartParkingAPI/Data/DTO/GarageDTO.cs
@@ -5,7 +5,7 @@ public class GarageDTO
     public string Name { get; set; }
     public string Address { get; set; }
     public string City { get; set; }
-    public int? OwnerId { get; set; }    
+    public int? OwnerId { get; set; } = null;   
     public double latitude { get; set; }
     public double longitude { get; set; }
 }
